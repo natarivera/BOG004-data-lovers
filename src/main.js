6a44b9e8560f0872370ea70c5ef27419cae6b551
Nataly Escobar <natalyescobar85@gmail.com>
2022-02-11 08:30:56 -0500

    agregando tabla de datos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,37 @@
 import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/athletes/athletes.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, data);
+
+var athletes = data.athletes;
+
+const rootElement  = document.getElementById("tabla_atletas");// Trayendo el elemento tabla_atletas (html)
+data.athletes.forEach(// Recorriendo el arreglo athletes
+    (athlete, index)=>{   
+
+        var td = document.createElement("td");//Creando un div     
+        td.innerHTML = athlete.name;//Creando un div
+        
+        var td2 = document.createElement("td");//Creando un div
+        td2.innerHTML = athlete.team;
+
+        var td3 = document.createElement("td");//Creando un div
+        td3.innerHTML = athlete.noc;
+
+        var td4 = document.createElement("td");//Creando un div
+        td4.innerHTML = athlete.sport;
+
+        var tr = document.createElement("tr");
+       
+
+        tr.appendChild(td);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
+    
+        rootElement.appendChild(tr);
+    }
+    
+);
