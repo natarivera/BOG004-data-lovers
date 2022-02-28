d6e928198375949c5a6e955f9cc112be5e367c85
Milates12 <accscortes09@gmail.com>
2022-02-28 08:20:39 -0500

    Colocando comentarios

--- src/data.js
+++ src/data.js
@@ -9,11 +9,13 @@ export const getByGender = (data,gender) => {
   return genderFilterData;
 };
 
-
+//Aplica los dos filtros por country y por gender
 export const filterData = (data, country, gender) =>{
+  //Si esta definido y es diferente de _all
   if (country && country !== "_all") {
     data = getByCountry(data, country);
   }
+  //Si gender esta definido y es diferente de _all
   if (gender && gender !== "_all") {
     data = getByGender(data, gender);
   }
@@ -21,15 +23,11 @@ export const filterData = (data, country, gender) =>{
 }
 
 /**
- * Listado de ciudades ordenadas
+ * Listado de Paises ordenados
  */
 export const getByCountries = (data) =>{
   let dataTeam = data.map((ele) => ele.team);
   let sortData = Array.from(new Set(dataTeam.sort()));
   return sortData;
 }
-
-
-
-
 
